1374af11d1e5b326ca52fe02d14b5eeb14ab5a99
Swiiish21 <malcolmotieno@gmail.com>
2024-10-25 14:07:55 +0300

    first commit

---
+++ customSort.py
@@ -0,0 +1,28 @@
+#below is  custom sort functions that take in a list of dictionaries and return the sorted list of dictionaries based on the 'marks' key in the dictionary
+#the first one is in ascending order and the second one is in descending order.
+
+def sort_dict_list(dict_list, key): # define a function and assign 2 parametres , one for the list of dictionaries and one for the key
+    return sorted(dict_list, key=lambda x: x[key]) # here we use the sorted function to sort the list of dictionaries based on the key
+
+dict_list = [ # create a list of dictionaries of a student and their marks
+    {'name': 'John', 'score': 90},
+    {'name': 'Mike', 'score': 78},
+    {'name': 'Joe', 'score': 65},
+    {'name': 'Alice', 'score': 40}
+]
+
+sorted_list = sort_dict_list(dict_list, 'score') # we define a variable to store the sorted list 
+print(sorted_list) # print the sorted list
+
+def sort_dict_desc(dict_list1, key): # define a function and assign 2 parametres , one for the list of dictionaries and one for the key
+    return sorted(dict_list1, key=lambda x: x[key], reverse=True) # here it's the same as the above function, except we use the inbult reverse function to sort the list in descending order.
+
+dict_list1 = [  # create a list of dictionaries of a student and their marks
+    {'name': 'Flex', 'score': 91},
+    {'name': 'Eric', 'score': 82},
+    {'name': 'Joanna', 'score': 55},
+    {'name': 'Reagan', 'score': 20}
+]
+
+sorted_list1 = sort_dict_desc(dict_list1, 'score') # we define a variable to store the sorted list
+print(sorted_list1)# print the sorted list
